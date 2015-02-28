235e34742cbc4a0ec322e3555f09aa699df6f3f0
Jig <no+reply@jig>
2015-02-28 11:10:02 -0600

    Remove async dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ version = imp.load_source(
     'jig', os.path.join(here, 'src', 'jig', '__init__.py')).__version__
 
 install_requires = [
-    'GitPython>=0.3.2RC1',
+    'GitPython==0.3.2',
     'docutils>=0.9.1']
 
 # Shims for missing stuff in Python 2.6
